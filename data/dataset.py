f4cb1f81aaa9522400b54c2a16f4071d64b36610
Alexander Shevelev <alex.shevelev@hotmail.com>
2020-06-24 12:33:32 +0300

    define HairDataset

---
+++ data/dataset.py
@@ -0,0 +1,12 @@
+from torch.utils import data
+
+
+class HairDataset(data.Dataset):
+    def __init__(self) -> None:
+        pass
+
+    def __len__(self) -> int:
+        pass
+
+    def __getitem__(self, idx: int):
+        pass
