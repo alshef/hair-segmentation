7b62bf7abe11eb824607db73fcfd5d8bf431f401
Alexander Shevelev <alex.shevelev@hotmail.com>
2020-06-26 10:58:59 +0300

    add jpegcompression augmentation

--- data/transforms.py
+++ data/transforms.py
@@ -14,6 +14,7 @@ def get_train_transforms(dataset_name: str,
         resizer,
         A.HorizontalFlip(p=0.5),
         A.Rotate(limit=5, p=0.5),
+        A.JpegCompression(quality_lower=80, quality_upper=100)
     ])
 
     image_transforms = T.Compose([
