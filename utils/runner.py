1f7d2ba4e3b6bb7af3ec4c95c432df8504282999
Alexander Shevelev <alex.shevelev@hotmail.com>
2020-06-27 11:54:39 +0300

    implement init and fix seeds methods

--- utils/runner.py
+++ utils/runner.py
@@ -1,12 +1,19 @@
-from pathlib import Path
 import sys
 sys.path.append("..")
+from pathlib import Path
+import random
+
+import torch
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
 
 
 class Trainer:
     def __init__(self, config: dict, experiment_path: Path) -> None:
         self.exp_path = experiment_path
         self.config = config
+        self.writer = SummaryWriter(experiment_path)
+        self._fix_all_seeds(config["seed"])
 
     def run(self) -> None:
         pass
@@ -16,3 +23,10 @@ class Trainer:
 
     def validate(self):
         pass
+
+    def _fix_all_seeds(self, seed: int = 42) -> None:
+        torch.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        np.random.seed(seed)
+        random.seed(seed)
