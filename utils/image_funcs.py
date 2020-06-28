8f3b5fa0e0941823f6cd39c541aafc0f84e44a22
Alexander Shevelev <alex.shevelev@hotmail.com>
2020-06-28 16:11:13 +0300

    implement function to convert logits to mask the same size as raw image

--- utils/image_funcs.py
+++ utils/image_funcs.py
@@ -2,6 +2,8 @@ from typing import Tuple
 from pathlib import Path
 
 from PIL import Image, ImageOps
+import torch
+import torchvision.transforms as transforms
 
 
 def pad_image_for_model(image, model_depth: int = 5, fill_const: int = 0) -> Image:
@@ -46,3 +48,21 @@ def prepare_image(image_path: Path) -> Tuple[Image, Tuple[int, int], Tuple[int, 
 
     out_image = pad_image_for_model(image)
     return out_image, raw_size, new_size, raw_image
+
+
+def make_mask_from_logits(logits: torch.Tensor,
+                          raw_size: Tuple[int, int],
+                          size_after_resizing: Tuple[int, int],
+                          threshold: float = 0.5) -> Image:
+    predicts = torch.sigmoid(logits)[0, 0]
+    predicts[predicts > threshold] = 1
+    predicts[predicts < threshold] = 0
+
+    pred_transforms = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.CenterCrop(size_after_resizing[::-1]),
+        transforms.Resize(raw_size[::-1], interpolation=2)
+    ])
+
+    mask = pred_transforms(predicts)
+    return mask
